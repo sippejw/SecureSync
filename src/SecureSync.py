85559767199cc1188b8dae929045f8ff2a952af7
Jackson Sippe <jackson123198@gmail.com>
2019-10-13 17:18:03 -0400

    Implements file and cli

--- src/SecureSync.py
+++ src/SecureSync.py
@@ -1,5 +1,10 @@
 import click
 import os
+import wormhole
+import json
+from twisted.internet import reactor
+from twisted.internet.defer import inlineCallbacks
+from random_word import RandomWords
 
 
 @click.group()
@@ -9,34 +14,69 @@ def cli():
 
 @click.command()
 def init():
-    click.echo("Initializing")
     if os.path.isfile('.SecureSync'):
         click.echo('This directory is already linked to a SecureSync instance!')
         return
+    generator = RandomWords()
+    keys = generator.get_random_words(maxLength=5)
+    keystring = "secure"
+    for key in keys:
+        keystring += "-" + key
+
+    config = {
+        "appid": "https://github.com/sippejw/SecureSync",
+        "relay_url": "ws://relay.magic-wormhole.io:4000/v1",
+        "key": keystring
+    }
+    configfile = open(".SecureSync", "w+")
+    configfile.write(json.dumps(config))
+    click.echo("Successfully initialized!")
+    click.echo("When connecting use the following key: " + keystring)
 
 
 @click.command()
 def connect():
-    click.echo("Connecting!")
     if os.path.isfile('.SecureSync'):
         click.echo('This directory is already linked to a SecureSync instance!')
         return
+    keystring = input('Please input key given by init: ')
+    config = {
+        "appid": "https://github.com/sippejw/SecureSync",
+        "relay_url": "ws://relay.magic-wormhole.io:4000/v1",
+        "key": keystring
+    }
+    configfile = open(".SecureSync", "w+")
+    configfile.write(json.dumps(config))
+    configfile.close()
+    click.echo("Connected!")
 
 
 @click.command()
+@inlineCallbacks
 def sync():
     click.echo("Syncing!")
     try:
-        f = open('.SecureSync')
-        f.close()
+        configfile = open('.SecureSync', 'r')
+        config = json.loads(configfile.read())
+        configfile.close()
+        appid = config['appid']
+        relay_url = config['relay_url']
     except IOError:
         print('This directory is not linked to a SecureSync instance!')
         return
+    connection = wormhole.create("https://github.com/sippejw/SecureSync", "ws://relay.magic-wormhole.io:4000/v1", reactor)
+    connection.allocate_code()
+    code = yield connection.get_code()
+    print("code:", code)
+    connection.send_message(b"outbound data")
+    inbound = yield connection.get_message()
+    yield connection.close()
 
 
 cli.add_command(init)
 cli.add_command(connect)
 cli.add_command(sync)
+
 
 if __name__ == '__main__':
     cli()
